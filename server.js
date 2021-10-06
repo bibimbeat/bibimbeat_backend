ba4067dc8e72e88abda7d20e35898d9d9442920a
hedaNFT <hedanft.dev@gmail.com>
2021-10-06 16:22:05 +0900

    20211006 16:21 deleted test frontend

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const axios = require('axios');
 const fs = require('fs');
 const FormData = require('form-data');
 const multer = require('multer');
-const NFTStorage = require('nft.storage');
 
 const multerImage = multer({
     storage: multer.diskStorage({
@@ -22,39 +21,36 @@ const multerMusic = multer({
 });
 
 require("dotenv").config();
-
 app.use(express.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 app.get('/', (req, res) => {
     const url = `https://api.pinata.cloud/data/testAuthentication`;
 
     return axios.get(url, {
-            headers: {
-                pinata_api_key: process.env.PinataApiKey,
-                pinata_secret_api_key: process.env.PinataSecretKey
-            }
-        })
-        .then(function (response) {
-            console.log(response.data.message);
-            res.send(response.data.message);
-            //handle your response here
-        })
-        .catch(function (error) {
-            console.log(error);
-            //handle error here
-        });
+        headers: {
+            pinata_api_key: process.env.PinataApiKey,
+            pinata_secret_api_key: process.env.PinataSecretKey
+        }
+    }).then(function (response) {
+        console.log(response.data.message);
+        res.send(response.data.message);
+        //handle your response here
+    }).catch(function (error) {
+        console.log(error);
+        //handle error here
+    });
 });
 
-app.post('/pinMusicSourceToIPFS', multerImage.single("music"), (req, res) => {
+app.post('/pinMusicSourceToIPFS', multerMusic.single("music"), (req, res) => {
     const url = "https://api.pinata.cloud/pinning/pinFileToIPFS";
     let data = new FormData();
-    let fsData = fs.createReadStream('./ImageToUpload/'+ req.file.filename);
+    let fsData = fs.createReadStream('./MusicToUpload/' + req.file.filename);
     data.append('file', fsData);
     return axios.post(url, data, {
         maxBodyLength: 'Infinity',
@@ -63,13 +59,11 @@ app.post('/pinMusicSourceToIPFS', multerImage.single("music"), (req, res) => {
             pinata_api_key: process.env.PinataApiKey,
             pinata_secret_api_key: process.env.PinataSecretKey
         }
-    })
-    .then(function (response) {
+    }).then(function (response) {
         console.log(response.data);
-        fs.unlink('./ImageToUpload/' + req.file.filename, (err) => err ? console.log(err) : console.log("파일을 삭제했습니다."));
-        res.send("Pinned album cover to IPFS!");
-    })
-    .catch(function (error) {
+        fs.unlink('./MusicToUpload/' + req.file.filename, (err) => err ? console.log(err) : console.log("deleted mp3 file."));
+        res.send(response.data.IpfsHash);
+    }).catch(function (error) {
         console.log(error);
     });
 });
@@ -77,7 +71,7 @@ app.post('/pinMusicSourceToIPFS', multerImage.single("music"), (req, res) => {
 app.post('/pinAlbumCoverToIPFS', multerImage.single("image"), (req, res) => {
     const url = "https://api.pinata.cloud/pinning/pinFileToIPFS";
     let data = new FormData();
-    let fsData = fs.createReadStream('./ImageToUpload/'+ req.file.filename);
+    let fsData = fs.createReadStream('./ImageToUpload/' + req.file.filename);
     data.append('file', fsData);
 
     return axios.post(url, data, {
@@ -87,54 +81,31 @@ app.post('/pinAlbumCoverToIPFS', multerImage.single("image"), (req, res) => {
             pinata_api_key: process.env.PinataApiKey,
             pinata_secret_api_key: process.env.PinataSecretKey
         }
-    })
-    .then(function (response) {
+    }).then(function (response) {
         console.log(response.data);
-        fs.unlink('./ImageToUpload/' + req.file.filename, (err) => err ? console.log(err) : console.log("파일을 삭제했습니다."));
-        res.send("Pinned album cover to IPFS!");
-    })
-    .catch(function (error) {
+        fs.unlink('./ImageToUpload/' + req.file.filename, (err) => err ? console.log(err) : console.log("deleted image file."));
+        res.send(response.data.IpfsHash);
+    }).catch(function (error) {
         console.log(error);
     });
 });
 
-app.post('/pinJsonFileToIPFS', (req, res, JSONBody) => {
+app.post('/pinJsonFileToIPFS', (req, res) => {
     const url = 'https://api.pinata.cloud/pinning/pinJSONToIPFS';
-    let data = new FormData();
-    data.append('file', fs.createReadStream('./axol.jpeg'));
-
-    const metadata = JSON.stringify({
-        name: 'Axol fish :)',
+    
+    return axios.post(url, req.body, {
+        maxBodyLength: 'Infinity',
+        headers: {
+            pinata_api_key: process.env.PinataApiKey,
+            pinata_secret_api_key: process.env.PinataSecretKey
+        }
+    }).then(function (response) {
+        console.log(response.data);
+        res.send(response.data.IpfsHash);
+    }).catch(function (error) {
+        console.log(error);
     });
-    data.append('pinataMetadata', metadata);
-
-
-    const JsonTest = JSON.stringify({
-        name: "hi Axol :)))"
-    });
-
-    return axios
-        .post(url, JsonTest, {
-            maxBodyLength: 'Infinity',
-            headers: {
-                pinata_api_key: process.env.PinataApiKey,
-                pinata_secret_api_key: process.env.PinataSecretKey
-            }
-        })
-        .then(function (response) {
-            console.log(response.data);
-            res.send("Pinned JSON file to IPFS!");
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
 });
-
-// app.post('/pinJsonFileToIPFS', (req, res) => {
-//     const url = 'https://api.pinata.cloud/pinning/pinJSONToIPFS';
-//     res.send();
-//     // const metadata = JSON.stringify(req.body);
-// });
 
 app.post('/pinFileToIPFS', (req, res) => {
     const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
@@ -177,13 +148,11 @@ app.post('/pinFileToIPFS', (req, res) => {
             pinata_api_key: process.env.PinataApiKey,
             pinata_secret_api_key: process.env.PinataSecretKey
         }
-    })
-    .then(function (response) {
+    }).then(function (response) {
         console.log(response.data);
         res.send();
         //handle your response here
-    })
-    .catch(function (error) {
+    }).catch(function (error) {
         console.log(error);
         //handle error here
     });
