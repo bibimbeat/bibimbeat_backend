28bef728aa0a4d6dd779e877ae41911026a49fad
hedaNFT <hedanft.dev@gmail.com>
2021-12-02 18:48:31 +0900

    commit for test beta

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://bibimbeat.azurewebsites.net");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -162,5 +163,5 @@ app.post('/pinFileToIPFS', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at https://bibimbeat-backend.azurewebsites.net`)
 })
